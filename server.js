1b10206a0b4c7c92c7ef06e712d11e307dd7b9ed
NathanM <nathan.milburn@outlook.com>
2022-03-08 19:38:17 -0800

    updated server.js files and get functions

--- server.js
+++ server.js
@@ -19,3 +19,11 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/assets/notes.html'))
 );
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
