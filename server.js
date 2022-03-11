e3286a6ac67cb969513811c694c7e5cf12b64449
NathanM <nathan.milburn@outlook.com>
2022-03-10 22:49:13 -0800

    removed the clog module calls. Might download to my package.json later, but ommitting it for now

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
 const api = require('./routes/apiRoutes.js'); 
 const PORT = process.env.port || 3001;
 
 const app = express();
-
-app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
