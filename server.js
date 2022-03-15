3d68c0083be4c9a14a8871b5f443c083c8d35b95
NathanM <nathan.milburn@outlook.com>
2022-03-14 20:18:18 -0700

    reformmated app.use calls

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api/notes', api);
 
 
